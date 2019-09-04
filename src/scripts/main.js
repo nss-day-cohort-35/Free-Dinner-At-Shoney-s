61a61a2594c4deea176f4c8515ba06292e623922
Heidi Smith <Heidi@LKs-MacBook-Pro.local>
2019-09-04 14:52:16 -0500

    accepted changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,5 @@
-<<<<<<< HEAD
-// objectData = {
-//     function => {
-//         fetchData
-//     }
-// }
-
-// Results: detect DataCue.js object ObjectListener
-// results.jsObject = {
-//     function(dataFunction)
-//     for (data lenght)
-//     results = AudioListener.listData[i]
-// }
-=======
 const restaurantData = Object.create(restaurantAPI);
 const resultLister = Object.create(resultPasser);
 
 restaurantData.fetchRestaurants()
 .then(parsedData => resultLister.fillArticle(parsedData))
->>>>>>> master
