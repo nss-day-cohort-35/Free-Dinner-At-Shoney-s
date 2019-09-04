a0571e902208eae72b5e109df461f9f4352f5950
Heidi Smith <Heidi@LKs-MacBook-Pro.local>
2019-09-04 14:38:53 -0500

    second commit

---
+++ src/scripts/main.js
@@ -0,0 +1,12 @@
+// objectData = {
+//     function => {
+//         fetchData
+//     }
+// }
+
+// Results: detect DataCue.js object ObjectListener
+// results.jsObject = {
+//     function(dataFunction)
+//     for (data lenght)
+//     results = AudioListener.listData[i]
+// }
