cf1c4910ac4be8691dd748d26100659de0ad04e9
Heidi Smith <Heidi@LKs-MacBook-Pro.local>
2019-09-05 13:34:43 -0500

    second commit

--- src/scripts/concerts.js
+++ src/scripts/concerts.js
@@ -1,28 +1,28 @@
-fetch("https://app.ticketmaster.com/discovery/v1/events.json?apikey=XM43D9BduVA5MvbUW1duD6DHBAOv563h")
+
+function lookup (searchTerm){
+fetch(`https://app.ticketmaster.com/discovery/v2/events/?keyword=${searchTerm}&city=nashville&apikey=XM43D9BduVA5MvbUW1duD6DHBAOv563h`)
     .then(concerts => concerts.json())
-    .then(parsedConcerts => {
-      
-            for (let i = 0; i < 9; i++){
-                let tickets = parsedConcerts._embedded.events[i].name;
-                 //concertInfo.innerHTML += createConcertList(name[i]); 
-                console.log(parsedConcerts._embedded.events[i].name) 
-                
+    .then(array => {
+            array.forEach(parsedConcerts._embedded.events)
+            document.querySelector(".dom").innerHTML += createConcertList(_embedded.events[i]);
+    console.log(parsedConcerts._embedded.events[i].name) 
+               
             }
         })
-
+   
    const concertInfo = document.querySelector("#concertInfo");
 
 
-   const createConcertList = (concert) => {
+   const createConcertList = (item) => {
         return `
         <div>
-            <h2>${concert.name}</h2>
-            <p>${concert.localDate}</p>
-            <p>${concert.localTime}</p>
-            <p>${concert.venue}</p>
+            <h2>${item.name}</h2>
         </div>
         `
     }
     console.log("HEY");
 
- 
+ lookup("football");
+
+
+    
