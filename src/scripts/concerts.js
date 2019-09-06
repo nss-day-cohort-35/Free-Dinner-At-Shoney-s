fdf5af6851b9207197963373b704116d2c1a92db
Heidi Smith <Heidi@LKs-MacBook-Pro.local>
2019-09-06 09:43:47 -0500

    3

--- src/scripts/concerts.js
+++ src/scripts/concerts.js
@@ -6,23 +6,26 @@ search.addEventListener("click", () => {
     console.log("listening");
 })
 
+
 fetch(`https://app.ticketmaster.com/discovery/v2/events/?keyword=&city=nashville&apikey=XM43D9BduVA5MvbUW1duD6DHBAOv563h`)
     .then(concerts => concerts.json())
     .then(parsedConcerts => {
 
         for (let i = 0; i < 19; i++) {
             document.querySelector(".dom").innerHTML += createConcertList(parsedConcerts._embedded.events[i]);
-            console.log(parsedConcerts._embedded.events[i].name)
+            console.log(parsedConcerts._embedded.events[i])
             //console.table(parsedConcerts);
         }
 
     })
 
+  
 
 const createConcertList = (item) => {
     return `
         <div>
             <h2>${item.name}</h2>
+            <a href="${item.url}">Get Tickets!</a>
         </div>
         `
 }
