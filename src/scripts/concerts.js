610777ab26f93ad870411aadb8c354d17bffd5ef
Heidi Smith <Heidi@LKs-MacBook-Pro.local>
2019-09-06 13:18:39 -0500

    3+

--- src/scripts/concerts.js
+++ src/scripts/concerts.js
@@ -1,12 +1,17 @@
 
+
 const concertAPI = function() {
+    const resultsDOM = document.querySelector(".dom"); 
+    while (resultsDOM.firstChild) { // Clears Results article for new results
+        resultsDOM.removeChild(resultsDOM.firstChild);
+      }
 fetch(`https://app.ticketmaster.com/discovery/v2/events/?city=Nashville,TN&keyword=concerts&apikey=XM43D9BduVA5MvbUW1duD6DHBAOv563h`)
 //fetch(`https://app.ticketmaster.com/discovery/v2/events/?keyword=&city=nashville&apikey=XM43D9BduVA5MvbUW1duD6DHBAOv563h`)
     .then(concerts => concerts.json())
     .then(parsedConcerts => {
 
         for (let i = 0; i < 19; i++) {
-            if (parsedConcerts._embedded.events[i].name != undefined) {
+            if (parsedConcerts._embedded.events[i] != undefined) {
             document.querySelector(".dom").innerHTML += createConcertList(parsedConcerts._embedded.events[i]);}
             console.log(parsedConcerts._embedded.events[i])
             //console.table(parsedConcerts);
@@ -26,8 +31,8 @@ const createConcertList = (item) => {
 
 }
 
-
 const search = document.getElementById("search");
 
 search.addEventListener("click", concertAPI)
-  
+ 
+//<button="${item.button}">Save</button>
