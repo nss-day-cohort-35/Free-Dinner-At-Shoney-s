a124607e23fc0dfcb76985713b7ec4ee61792bf6
FortunatoMugnano <fortunato.mugnano88@gmail.com>
2019-09-08 14:48:58 -0500

    finally all the meetups are showing in the dom

--- src/scripts/events.js
+++ src/scripts/events.js
@@ -1,85 +1,30 @@
-console.log("this is working")
-
-var resizeIt = [];
-var localListName = [];
 
 
-
-const API = {
-    askForApi() {
-        return fetch("https://www.eventbriteapi.com/v3/events/search/?q=senior&location.address=nashville&token=FZEHRXINGLKISD4D62G7", {
-            "headers": {
-                "Accept": "application/json"
-            }
-        })
-            .then(Response => Response.json())
-            .then(parsedDate => {
-
-
-
-                for (i = 0; i < parsedDate.events.length; i++) {
-                    localListName.push(parsedDate.events[i].name.text)
-                    console.log("list of name", localListName)
-                }
-
-                let localListDate = [];
-                for (i = 0; i < parsedDate.events.length; i++) {
-                    localListDate.push(parsedDate.events[i].start.local)
-
-                }
-                console.log("local list", localListDate)
-
-                for (let i = 0; i < localListDate.length; i++) {
-
-                    let y = localListDate[i].slice(0, 10)
-                    resizeIt.push(y)
-                    console.log(resizeIt)
-                }
-
-
-
-
-            })
-    }
+function domHtml(data) {
+    return `<h3>${data.name.text}<h3>
+    <input type = "button" id = "save-${data.name.text}" class = "saveButton" value="Save">`
 }
 
-console.log(localListName[0])
 
+//function results(array) {
+  //  array.forEach(data => {
+    //    document.querySelector(".dom").innerHTML += domHtml(data);
+   // });
+// }
 
+function meetups(value) {
+    fetch(`https://www.eventbriteapi.com/v3/events/search/?q=${value}&location.address=nashville&token=FZEHRXINGLKISD4D62G7`)
+        .then(result => result.json())
+        .then(parsedResult => {
+            parsedResult.events.forEach(element => {
+                document.querySelector(".dom").innerHTML += domHtml(element);
+            });
 
-API.askForApi().then(localList => {
-    function noEvents() {
-        return `<h3> There are no events for this day, please try another one </h3>`
-    }
+        });
+}
 
-
-    function eventsHTML(item) {
-        return ` <li>
-    <h3><span>${item.events.name.text}</span><button class="save-button">Save</button></h3>
-    <p>Start: ${item.events.start.local}</p>
-    </li>`
-    }
-
-    const saveEvents = document.getElementById("result")
-
-
-
-
-
-
-    const buttonPress = document.querySelector("#save_search").addEventListener("click", function addResults() {
-
-        let tableValue = document.querySelector("#search_events").value
-        for (i = 0; i < resizeIt.length; i++) {
-            if (tableValue === resizeIt[i] && tableValue != undefined) {
-                saveEvents.innerHTML += eventsHTML(localListName[i])
-            } else if (tableValue === "") {
-                saveEvents.innerText += noEvents(API.askForApi)
-            }
-        }
-
-
-    })
-
-})
+document.querySelector("#search-meetUps").addEventListener("click", event => {
+    let searchTerm = document.querySelector("#fm-search-bar").value
+    meetups(searchTerm)
+});
 
