8e8f9eb96f3cfe22da36754925a28c3cf41fbb74
FortunatoMugnano <fortunato.mugnano88@gmail.com>
2019-09-08 13:42:50 -0500

    minor attempts to fix it

--- src/scripts/try.js
+++ src/scripts/try.js
@@ -1,5 +1,5 @@
 
-
+const apiEvents =  function() {
 fetch("https://www.eventbriteapi.com/v3/events/search/?q=senior&location.address=nashville&token=FZEHRXINGLKISD4D62G7", {
     "headers": {
         "Accept": "application/json"
@@ -8,12 +8,17 @@ fetch("https://www.eventbriteapi.com/v3/events/search/?q=senior&location.address
     .then(Response => Response.json())
     .then(parsedDate => {
 
-        
         console.table(parsedDate)
-        printDomFunction(parsedDate)
+        for (let i = 0; i < parsedDate.events.lenght; i++) {
+            console.log(parsedDate.events[i])
+            if (parsedDate.events[i].name.text != undefined) {
+            document.querySelector("#eventsList").innerHTML += eventsHTML(parsedDate.events[i].name.text);}
+            
+            
+        }
     })
 
-
+}
 function eventsHTML(item) {
     return ` <li>
     <h3><span>${item.events.name.text}</span><button class="save-button">Save</button></h3>
@@ -21,8 +26,7 @@ function eventsHTML(item) {
     </li>`
 }
 
-const eventsContainer = document.querySelector(".eventsList")
 
-const printDomFunction = (data) => {
-    eventsContainer.innerHTML += eventsHTML(data)
-}
+const searchEvents = document.querySelector("#save_search")
+
+searchEvents.addEventListener("click", apiEvents)
